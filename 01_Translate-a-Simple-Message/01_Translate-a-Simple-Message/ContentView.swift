9f7edcc8253a2d9cbac384ae6cce4a4cb2e4312b
Ashok VJ <vardhan4all@gmail.com>
2025-10-16 20:58:03 +0530

    excercise1 Translate a Simple Message

---
+++ 01_Translate-a-Simple-Message/01_Translate-a-Simple-Message/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  01_Translate-a-Simple-Message
+//
+//  Created by Ashok Vardhan Jangeti on 16/10/25.
+//
+
+import SwiftUI
+import Translation
+
+struct ContentView: View {
+    @State private var showTranslation = false
+    @State private var inputText: String = ""
+    @State private var translatedText: String?
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            TextField("Enter text", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            Button("Translate") {
+                showTranslation.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+            .disabled(inputText.isEmpty)
+        }
+        .padding()
+        .translationPresentation(isPresented: $showTranslation, text: inputText) {
+            translatedText in
+            inputText = translatedText
+        }
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
